94c02667a06c74a5b9a7bfa65510d3a27de1cc1d
Akshay Lokray <akshay_lr@Akshays-MacBook-Pro.local>
2019-12-31 11:03:42 +0530

    Updates

---
+++ precache-manifest.fb7ae7cefa3d0952212137ff1f240293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f700c895415d242b110604487b632fb8",
+    "url": "/todo-application/index.html"
+  },
+  {
+    "revision": "af1c59d72351f3d5e7ae",
+    "url": "/todo-application/static/css/main.4c4e00cc.chunk.css"
+  },
+  {
+    "revision": "153dec24d7444a6c2378",
+    "url": "/todo-application/static/js/2.72038e2f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-application/static/js/2.72038e2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af1c59d72351f3d5e7ae",
+    "url": "/todo-application/static/js/main.9552cbbe.chunk.js"
+  },
+  {
+    "revision": "9c495c92646308d250e7",
+    "url": "/todo-application/static/js/runtime-main.d32c5c84.js"
+  }
+]);
